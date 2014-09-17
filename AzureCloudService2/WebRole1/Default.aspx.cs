2916b1566b85b0e1e2a80d562d0b4f15b951971b
WinWrap <ww2008a@gmail.comx>
2014-09-17 07:10:43 -0700

    a web role example

---
+++ AzureCloudService2/WebRole1/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebRole1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (var basicNoUIObj = new WinWrap.Basic.BasicNoUIObj())
+            {
+                basicNoUIObj.Secret = new Guid(Utils.GetPatternString("wwwr140916b", "Guid[(]\"(.*)\"[)]"));
+                basicNoUIObj.Initialize();
+                Button1.Text = basicNoUIObj.Evaluate("2+3");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
