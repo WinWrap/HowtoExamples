39d23c8fea0377aa76cdf84ec28315794a5018de
WinWrap <ww2008a@gmail.comx>
2014-09-23 17:47:01 +0000

    added test website

---
+++ ww-ws-test/ww-ws-test/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ww_ws_test
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = System.DateTime.Now.ToString();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (var basicNoUIObj = new WinWrap.Basic.BasicNoUIObj())
+            {
+                basicNoUIObj.Secret = new Guid(Utils.GetPatternString("ww-ws-test", "Guid[(]\"(.*)\"[)]"));
+                basicNoUIObj.Initialize();
+                Label1.Text = basicNoUIObj.Evaluate("System.DateTime.Now.ToString()");
+            }
+        }
+    }
+}
