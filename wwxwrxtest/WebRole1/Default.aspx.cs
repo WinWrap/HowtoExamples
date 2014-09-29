8e5f3e3a45a2e777921ee6d4738ad7b6a9917ebe
WinWrap <ww2008a@gmail.comx>
2014-09-29 05:07:39 -0700

    working minimal webrole with WinWrap

--- wwxwrxtest/WebRole1/Default.aspx.cs
+++ wwxwrxtest/WebRole1/Default.aspx.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 
+/*
+ * Publish cloud service (wwxwrxtest):
+ *   1a) fully deleted service, webrole, deployment, and storage
+ *   1b) new publish profile (manage and remove existing profile)
+ *   1c) use "North Central US", Locally Redundant
+ *   or
+ *   2a) running service, et.
+ *   2b) existing publish profile
+ */
+
 namespace WebRole1
 {
     public partial class Default : System.Web.UI.Page
@@ -14,8 +24,10 @@ namespace WebRole1
             //Button1.Text = DateTime.Now.ToString();
             using (var basicNoUIObj = new WinWrap.Basic.BasicNoUIObj())
             {
+                basicNoUIObj.Secret = new Guid(Utils.GetPatternString("wwxwrxtest", "Guid[(]\"(.*)\"[)]"));
                 basicNoUIObj.Initialize();
-                Button1.Text = basicNoUIObj.Evaluate("4+3");
+                //Button1.Text = basicNoUIObj.Evaluate("4+3");
+                Button1.Text = basicNoUIObj.Evaluate("System.DateTime.Now.ToString()");
             }
         }
     }
