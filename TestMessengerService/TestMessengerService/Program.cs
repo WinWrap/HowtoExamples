bd7924c7df59bc602b5f1b76b53b86db9627d7d0
WinWrap <ww2008a@gmail.comx>
2014-10-02 19:14:04 +0000

    Force wwxwrxtest WebRole to run in a 32 bit process.

--- TestMessengerService/TestMessengerService/Program.cs
+++ TestMessengerService/TestMessengerService/Program.cs
@@ -19,7 +19,7 @@ namespace TestMessengerService
 
             List<Thread> threads = new List<Thread>();
 
-            for (int i = 0; i < 10; ++i)
+            for (int i = 0; i < 100; ++i)
             {
                 Thread thread = new Thread(SendMessageThread);
                 thread.Start();
