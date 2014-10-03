a8750a03a10b855b36de77d4cddaf45a1e34b01f
WinWrap <ww2008a@gmail.comx>
2014-10-03 09:06:02 -0400

    create initial ww-ws-objmod
    will show ScriptableObjectModel use

---
+++ ww-ws-objmod/ww-ws-objmod/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * http://ww-ws-objmod.azurewebsites.net/
+ */
+
+namespace ww_ws_objmod
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Button1.Text = DateTime.Now.ToString();
+        }
+    }
+}
