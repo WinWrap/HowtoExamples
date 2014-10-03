1f09693591e3e2ee3bee7c49aa785501d3233a2b
WinWrap <ww2008a@gmail.comx>
2014-10-03 09:26:33 -0400

    add WinWrap

--- ww-ws-objmod/ww-ws-objmod/Default.aspx.cs
+++ ww-ws-objmod/ww-ws-objmod/Default.aspx.cs
@@ -10,7 +10,12 @@ namespace ww_ws_objmod
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            using (var basicNoUIObj = new WinWrap.Basic.BasicNoUIObj())
+            {
+                basicNoUIObj.Secret = new Guid(Utils.GetPatternString("ww-ws-objmod", "Guid[(]\"(.*)\"[)]"));
+                basicNoUIObj.Initialize();
+                Button1.Text = basicNoUIObj.Evaluate("2+3");
+            }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
